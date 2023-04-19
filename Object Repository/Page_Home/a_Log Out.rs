14f3de287bb9975f1df094d9947aca3cdbf6a491
DELL <DELL@DESKTOP-ICU3MKD>
2023-04-20 00:09:19 +0530

    login

---
+++ Object Repository/Page_Home/a_Log Out.rs
@@ -0,0 +1,139 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Log Out</name>
+   <tag></tag>
+   <elementGuidId>363fe8ac-e8ab-4bfc-acd7-5d177cb17776</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>a[title=&quot;Signout&quot;]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//li[@id='dropdownLink1']/ul/li[3]/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>dac320f4-9660-450c-836b-0ca8bc44c107</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>title</name>
+      <type>Main</type>
+      <value>Signout</value>
+      <webElementGuid>8be60332-0a52-4f49-ac48-3e274eb4158d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/index.php?r=auth/logout</value>
+      <webElementGuid>202be854-37bd-4f24-8d54-e3928874efd0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value> Log Out</value>
+      <webElementGuid>74d15aa6-253e-447c-8c62-d45686170b82</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;dropdownLink1&quot;)/ul[@class=&quot;dropdown-menu dropdown-menu-right fadeInUp animated&quot;]/li[3]/a[1]</value>
+      <webElementGuid>e152c8c9-7cf9-4420-9319-d22d016a4a79</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//li[@id='dropdownLink1']/ul/li[3]/a</value>
+      <webElementGuid>ca40aa76-f22d-40ec-a70a-a74fc76df123</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Log Out')]</value>
+      <webElementGuid>eafa67d6-fa92-4347-b336-1db574da264d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Profile'])[1]/following::a[1]</value>
+      <webElementGuid>9f56d4dc-d73f-42f4-914b-cfbf01b10daf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='jylnlgd8q8h7xrlu2g8p0ebggv910ixenck9e6vwk g2y1pthlullk14p76tc24rftk8de4v1rrpomijz9o'])[1]/following::a[2]</value>
+      <webElementGuid>74e1d21d-ec72-4bdb-9246-1a981c0ac2b8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='×'])[1]/preceding::a[1]</value>
+      <webElementGuid>21efcb1f-9bfa-456c-8a7b-4cb0d57b40f4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Advance Search'])[1]/preceding::a[1]</value>
+      <webElementGuid>3748f853-0d1f-4add-b6fe-23cca216eb94</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Log Out']/parent::*</value>
+      <webElementGuid>69054258-a8eb-4d18-b242-d299cde60936</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/index.php?r=auth/logout')]</value>
+      <webElementGuid>61f7f38d-322b-47c5-a8b5-19ca5e1ab924</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[4]/ul/li[3]/a</value>
+      <webElementGuid>97bea12b-da2a-45e1-980e-12f4a660ee67</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@title = 'Signout' and @href = '/index.php?r=auth/logout' and (text() = ' Log Out' or . = ' Log Out')]</value>
+      <webElementGuid>4b5de4df-f6f3-4a31-ba01-468787ef9c8b</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_Home/img_Denosumab_img-circle profile-img thumb-midsm.rs
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>img_Denosumab_img-circle profile-img thumb-midsm</name>
+   <tag></tag>
+   <elementGuidId>33176203-006c-4e50-b82f-4baf40a50fa8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>img.img-circle.profile-img.thumb-midsm</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//li[@id='dropdownLink1']/a/img</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>img</value>
+      <webElementGuid>c0c80156-1452-4bf5-acf7-1f9f3fd6f93e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>img-circle profile-img thumb-midsm</value>
+      <webElementGuid>e9b65a35-7875-4245-9ba7-6d48416d0e5b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>src</name>
+      <type>Main</type>
+      <value>//sandoz-staging.integratedge.com/files/profilepic/1/ip_1_1442_641978e5f39c2.jpg</value>
+      <webElementGuid>2ad99d6c-ac55-4a25-89ee-e05f201ddff0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>alt</name>
+      <type>Main</type>
+      <value>User Pic</value>
+      <webElementGuid>e481b2fb-3a8c-4460-9946-6c9305a93183</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;dropdownLink1&quot;)/a[@class=&quot;dropdown-toggle clearfix&quot;]/img[@class=&quot;img-circle profile-img thumb-midsm&quot;]</value>
+      <webElementGuid>ff3597e8-eade-480d-a563-e907c006cc13</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//li[@id='dropdownLink1']/a/img</value>
+      <webElementGuid>41d5586c-f652-414d-b0f0-6bf9f3b9ffd0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:img</name>
+      <type>Main</type>
+      <value>//img[@alt='User Pic']</value>
+      <webElementGuid>5816113d-31df-468e-a8fa-3dc00c47c5f2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[4]/a/img</value>
+      <webElementGuid>7955a03c-6f0d-44e6-a3a0-e74fdb654cf5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//img[@src = '//sandoz-staging.integratedge.com/files/profilepic/1/ip_1_1442_641978e5f39c2.jpg' and @alt = 'User Pic']</value>
+      <webElementGuid>f9af9284-1e08-4c48-8937-41c9ab78fe38</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
